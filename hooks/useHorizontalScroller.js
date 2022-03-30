7e8ee5ce34a163fc6b49d581dce27e848a7d76b6
Shayan <ValiyariOffice@gmail.com>
2022-03-30 14:53:03 +0430

    Working on BG

--- hooks/useHorizontalScroller.js
+++ hooks/useHorizontalScroller.js
@@ -2,7 +2,8 @@ import { useRef, useEffect, useState } from 'react';
 export default function useHorizontalScroller(
   scrollerRef,
   progressBarRef,
-  scrollPathRef
+  scrollPathRef,
+  bgRef
 ) {
   const [completion, setCompletion] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
@@ -10,19 +11,42 @@ export default function useHorizontalScroller(
   let scroller = scrollerRef.current;
   let progressBar = progressBarRef.current;
   let scrollPath = scrollPathRef.current;
+  let bg = bgRef.current;
   useEffect(() => {
     if (!scroller) scroller = scrollerRef.current;
     if (!scrollPath) scrollPath = scrollPathRef.current;
 
+    if (!bg) bg = bgRef.current;
+    // console.log(scroller.scrollWidth, bg.getBoundingClientRect().width);
+    // bg.style.width = scroller.scrollWidth + 'px';
+    scroller.scrollTo({
+      left: 0,
+      behavior: 'smooth',
+    });
+    bg.scrollTo({
+      left: 0,
+      behavior: 'smooth',
+    });
+
+    let scrollProgress = Math.ceil(
+      (scroller.scrollLeft / (scroller.scrollWidth - scroller.clientWidth)) *
+        100
+    );
+    setCompletion(scrollProgress);
     const scrollHorizontally = (e) => {
       if (e.deltaY == 0) return;
       e.preventDefault();
-      // console.log(
-      //   Math.min(
-      //     scroller.scrollLeft + (e.deltaY * window.innerWidth) / 170,
-      //     scroller.scrollWidth - scroller.getBoundingClientRect().width - 1
-      //   )
-      // );
+      console.log(e.deltaY);
+      scrollProgress = Math.ceil(
+        (Math.min(
+          scroller.scrollLeft + (e.deltaY * window.innerWidth) / 170,
+          scroller.scrollWidth - scroller.getBoundingClientRect().width - 1
+        ) /
+          (scroller.scrollWidth - scroller.clientWidth)) *
+          100
+      );
+      setCompletion(scrollProgress);
+      
       scroller.scrollTo({
         left: Math.min(
           scroller.scrollLeft + (e.deltaY * window.innerWidth) / 170,
@@ -30,6 +54,31 @@ export default function useHorizontalScroller(
         ),
         behavior: 'smooth',
       });
+
+      if (e.deltaY > 0) {
+        if (
+          scroller.scrollLeft <
+          scroller.scrollWidth - scroller.getBoundingClientRect().width - 1
+        ) {
+          bg.scrollTo({
+            left: Math.min(
+              bg.scrollLeft + (e.deltaY * window.innerWidth) / 170,
+              bg.scrollWidth - bg.getBoundingClientRect().width - 1
+            ),
+            behavior: 'smooth',
+          });
+        }
+      } else {
+        if (completion > 0) {
+          bg.scrollTo({
+            left: Math.max(
+              bg.scrollLeft + (e.deltaY * window.innerWidth) / 170,
+              0
+            ),
+            behavior: 'smooth',
+          });
+        }
+      }
     };
 
     const updateScrollProgress = () => {
@@ -49,6 +98,15 @@ export default function useHorizontalScroller(
         left: targetScrollpx,
         behavior: 'smooth',
       });
+      const delta = completion - posXFact;
+
+      if (e.deltaY > 0) {
+        bg.scrollTo({
+          left: targetScrollpx,
+          behavior: 'smooth',
+        });
+      } else {
+      }
       if (progressBar) progressBar.style.width = posXFact + '%';
       // console.log(targetScrollpx);
     };
